a9d84e87dea1dc8c24287745adee82a3d3546386
KanishkSingh039 <kanishks039@gmail.com>
2026-06-24 06:46:42 +0530

    learning about the WebRTC

---
+++ backend/server.js
@@ -0,0 +1,33 @@
+const express = require("express");
+const app = express();
+const http = require("http");
+const server = http.createServer(app);
+const { Server } = require("socket.io");
+const io = new Server(server, {
+  cors: {
+    origin: "http://localhost:5173",
+  },
+});
+
+io.on("connection", (socket) => {
+  console.log("a user connected");
+    socket.on("offer", (offer) => {
+    // console.log("Received offer:", offer);
+    socket.broadcast.emit("offer-read", offer); // Broadcast the offer to all other connected clients
+    // Here you can handle the received offer, e.g., create an answer and send it back to the client
+  });
+  socket.on("answer", (answer) => {
+    // console.log("Received answer:", answer);
+    socket.broadcast.emit("answer-read", answer); // Broadcast the answer to all other connected clients
+  });
+  socket.on("ice-candidate", (candidate) => {
+    // console.log("Received ICE candidate:", candidate);
+    socket.broadcast.emit("ice-candidate-read", candidate); // Broadcast the ICE candidate to all other connected clients
+  });
+  socket.on("disconnect", () => {
+    console.log("user disconnected");
+  });
+});
+server.listen(3000, () => {
+  console.log("listening on *:3000");
+});
---
+++ frontend/src/App.jsx
@@ -0,0 +1,171 @@
+import {io} from 'socket.io-client'
+import { useState, useRef,useEffect } from 'react';
+function App() {
+  const [localStream, setLocalStream] = useState(null);
+  const [remoteStream, setRemoteStream] = useState(null);
+  const localVideoRef = useRef(null);
+  const remoteVideoRef = useRef(null);
+  const pc = useRef(null);
+  const socket = useRef(null);
+  const pendingCandidates = useRef([]);
+  const[receivedOffer,setReceivedOffer]=useState(null);
+  const[receivingCall,setReceivingCall]=useState(false);
+  useEffect(() => {
+    // console.log("Setting up peer connection and socket");
+    pc.current = new RTCPeerConnection({
+        iceServers:[
+          {
+            urls: "stun:stun.l.google.com:19302",
+          },
+        ],}
+    );
+        pc.current.onconnectionstatechange = () => {
+          // console.log("Connection:", pc.current.connectionState);
+        };
+
+        pc.current.oniceconnectionstatechange = () => {
+          // console.log("ICE:", pc.current.iceConnectionState);
+        };
+
+        // pc.current.ontrack = (event) => {
+        //   console.log("Received remote track:", event.streams[0]);
+        // };
+    socket.current = io("http://localhost:3000");
+    socket.current.on("connect",()=>{
+      console.log("Connected to signaling server");
+    });
+    socket.current.on("offer-read",async (offer)=>{
+      setReceivedOffer(offer);
+      setReceivingCall(true);
+      for (const candidate of pendingCandidates.current) {
+  await pc.current.addIceCandidate(
+    new RTCIceCandidate(candidate)
+  );
+}
+      pendingCandidates.current=[];
+    });
+    socket.current.on("answer-read",async (answer)=>{
+        await pc.current.setRemoteDescription(new RTCSessionDescription(answer))
+      });
+    socket.current.on("ice-candidate-read",async (candidate)=>{
+      try{
+        if(!pc.current.remoteDescription){
+          pendingCandidates.current.push(candidate);
+        }
+        else
+        {
+          await pc.current.addIceCandidate(
+            new RTCIceCandidate(candidate));
+
+        }
+      }catch(e){
+        console.error("Error adding received ice candidate",e);
+      }
+    });
+    pc.current.ontrack=(event)=>{
+        // console.log("track recieved");
+        
+        // console.log(event);
+        // console.log("Received remote track :", event.streams[0]);
+        remoteVideoRef.current.srcObject=event.streams[0];
+        setRemoteStream(event.streams[0]);
+      }
+    return () => {
+      socket.current.off("connect");
+      socket.current.off("offer-read");
+      socket.current.off("answer-read");
+      socket.current.off("ice-candidate-read");
+      if (pc.current) {
+        pc.current.close();
+      }
+      if (socket.current) {
+        socket.current.disconnect();
+      }
+    };
+  },[]);
+  async function handleOffer(offer){
+    // console.log("Handling answering");
+    const stream=await navigator.mediaDevices.getUserMedia(
+      {
+        video:true,
+        audio:true,
+      }
+    )
+    setLocalStream(stream);
+    localVideoRef.current.srcObject=stream;
+    stream.getTracks().forEach((track)=>{
+      pc.current.addTrack(track,stream);
+    })
+    await pc.current.setRemoteDescription(new RTCSessionDescription(offer));
+    for (const candidate of pendingCandidates.current) {
+      await pc.current.addIceCandidate(
+        new RTCIceCandidate(candidate)
+      );
+    }
+    pendingCandidates.current = [];
+    const answer=await pc.current.createAnswer();
+    await pc.current.setLocalDescription(answer);
+    socket.current.emit("answer",answer);
+    pc.current.onicecandidate=(event)=>{
+      if(event.candidate)
+      {
+        socket.current.emit("ice-candidate",event.candidate);
+      }
+    }
+    // pc.current.ontrack=(event)=>{
+    //     console.log("track recieved");
+
+    //   console.log(event);
+      
+    //   console.log("Received remote track at receiverside:", event.streams[0]);
+    //   remoteVideoRef.current.srcObject=event.streams[0];
+    //   setRemoteStream(event.streams[0]);
+    // }
+  }
+  async function Call()
+  {
+    // console.log("Calling");
+    const stream=await navigator.mediaDevices.getUserMedia(
+      {
+        video:true,
+        audio:true,
+      }
+    )
+    localVideoRef.current.srcObject=stream;
+    stream.getTracks().forEach((track)=>{
+      pc.current.addTrack(track,stream);
+    })
+    const offer=await pc.current.createOffer();
+    await pc.current.setLocalDescription(offer);
+    pc.current.onicecandidate=(event)=>{
+      if(event.candidate)
+        {
+          socket.current.emit("ice-candidate",event.candidate);
+        }
+      }
+      // pc.current.ontrack=(event)=>{
+      //   console.log("track recieved");
+        
+      //   console.log(event);
+      //   console.log("Received remote track at callerside:", event.streams[0]);
+      //   remoteVideoRef.current.srcObject=event.streams[0];
+      //   setRemoteStream(event.streams[0]);
+      // }
+      socket.current.emit("offer",offer);
+      setLocalStream(stream);
+    }
+  return ( 
+    <div>
+      <button onClick={()=>Call()}>Call</button>
+      {receivingCall && (
+        <div>
+          <button onClick={()=>handleOffer(receivedOffer)}>Answer</button>
+        </div>
+      )}
+      <video ref={localVideoRef} autoPlay playsInline></video>
+      <video ref={remoteVideoRef} autoPlay playsInline></video>
+    </div>
+   );
+}
+
+export default App;
